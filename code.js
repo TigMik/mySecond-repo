6186ae805526de975706f717b05ae34920986f2f
TigMik <tigranmika95@gmail.com>
2020-06-11 19:20:50 +0400

    some changes are done

--- code.js
+++ code.js
@@ -11,7 +11,7 @@ Write a loop that makes seven calls to console.log to output the following trian
 
 let x = "#";
 
-for (let y = 0; y < 7; y++) {
-  console.log(x);
-  x += "#";
+for (var y = 0; y < 7; y++) {
+    console.log(x);
+    x += "#";
 }
